e056ed2f714240d0b8cdbebb6bf955dd6dc50eef
surprise1161 <paulo.cezar622002@gmail.com>
2019-02-09 21:10:16 -0200

    Added owner cog

---
+++ modules/utils/owner.py
@@ -0,0 +1,11 @@
+from discord.ext import commands
+
+class Owner():
+    """Owner commands"""
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['die', 'kys'])
+    async def logout(self, ctx):
+        """Stop the process that bot's running"""
+        await ctx.invoke(self.bot.get_command('jsk py'), 'await _bot.logout()')

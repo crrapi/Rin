de77817ad6d83a5f7c2cd9810f772d9b7e9396db
surprise1161 <paulo.cezar622002@gmail.com>
2019-02-10 14:55:00 -0200

    Fixed zerochan

--- modules/utils/zerochan.py
+++ modules/utils/zerochan.py
@@ -1,11 +1,10 @@
 from . import scrap
 
 
+
 async def search(query: str, page: int):
     results = scrap.search(query, p=page)
-    images = []
-    for i in range(scrap.IMAGES_PER_TAG):
-        images.append(await results.__anext__())
+    images = [await results.__anext__() for i in range(scrap.IMAGES_PER_TAG)]
     return images
 
 
@@ -21,7 +20,5 @@ async def info(query: str):
 
 async def meta(query: str, page: int):
     meta_tags = scrap.meta(query, p=page)
-    results = []
-    for i in range(scrap.TAGS_PER_META):
-        results.append(await meta_tags.__anext__())
-    return results
+    tags = [await meta_tags.__anext__() for i in range(scrap.TAGS_PER_META)]
+    return tags

4c984d9b9488c4b3d60878f72f71bb7a7e7c9f6c
reformed5680 <paulo.cezar622002@gmail.com>
2019-03-01 23:50:55 -0300

    Rewrite Purge

--- modules/discord/moderation.py
+++ modules/discord/moderation.py
@@ -7,6 +7,11 @@ def check_amount(amount):
     if amount < 0 or amount > 500:
         raise commands.BadArgument('Input a amount that is higher than 0 or less than 500.')
 
+def check_message(message):
+    if author:
+        return msg.author.id == target.id
+    return True
+
 class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -15,14 +20,13 @@ class Moderation(commands.Cog):
     @commands.has_permissions(manage_messages=True)
     @commands.bot_has_permissions(manage_messages=True)
     @commands.cooldown(1, 5, commands.BucketType.user)
-    async def purge(self, ctx, amount: int = None):
+    async def purge(self, ctx, amount: int = None, member: discord.Member = None):
         """Delete messages by ammount"""
         exceptions = (commands.UserInputError, commands.BadArgument)
         try:
             check_amount(amount)
-            await ctx.message.delete()
-            await ctx.message.channel.purge(limit=amount)
-            await ctx.send(f'Deleted {int(amount)} messages.', delete_after=5)
+            deleted_msg = await ctx.channel.purge(limit=amount, check=msgcheck)
+            await ctx.send(f'Deleted {len(deleted)} messages.', delete_after=5)
         except exceptions as e:
             await ctx.send(e)
 

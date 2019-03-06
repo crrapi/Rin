a2af7ead72fe37c552fa25ec01783654f3b26001
reformed5680 <paulo.cezar622002@gmail.com>
2019-03-06 01:53:51 -0300

    Try/Except missing

--- modules/API/rin_aur.py
+++ modules/API/rin_aur.py
@@ -1,5 +1,7 @@
 import aiohttp
 from discord.ext import commands
+
+from ..utils import custom_exceptions
 from ..utils.paginator import Pages
 
 
@@ -11,13 +13,21 @@ class AUR(commands.Cog):
     @commands.command(aliases=['aurweb', 'rpc'])
     async def aur(self, ctx, *, query: str):
         """Search packages from AUR"""
+        query = query.lower()
         async with aiohttp.ClientSession() as session:
             async with session.get(f'https://aur.archlinux.org/rpc/?v=5&type=search&arg={query}') as response:
-                response = await response.json()
-                response = response['results']
-                names = [i['Name'] for i in response]
-                pages = Pages(ctx, lines=tuple(names))
-                await pages.paginate()
+                exceptions = (custom_exceptions.ResourceNotFound, Exception)
+                try:
+                    response = await response.json()
+                    response = response['results']
+                    if not response:
+                        raise custom_exceptions.ResourceNotFound('Results not found.')
+                    names = [i['Name'] for i in response]
+                    pages = Pages(ctx, lines=tuple(names))
+                    await pages.paginate()
+                except exceptions as e:
+                    await ctx.message.add_reaction('\U0000274c')
+                    await ctx.send(e, delete_after=10)
 
 
 def setup(bot):

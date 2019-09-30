14a448301746eb333eda63c37309f6ae3d13db58
Chris Rrapi <toadawes12@gmail.com>
2019-09-30 17:16:14 -0400

    Fix some things in main.py
    This includes moving `jishaku` into extensions, changing Game to Activity, showing exception on extension load failure, etc

--- main.py
+++ main.py
@@ -6,21 +6,15 @@ from discord.ext import commands
 with open('config.json') as file:
     config = json.load(file)
 
-
-async def get_prefix(_bot, message):
-    prefixes = ['rin ']
-
-    return commands.when_mentioned_or(*prefixes)(_bot, message)
-
-
 extensions = ['modules.API.rin_danbooru',
               'modules.API.rin_zerochan',
               'modules.API.rin_aur',
               'modules.discord.moderation',
               'modules.utils.errors',
-              'modules.utils.information']
+              'modules.utils.information',
+              'jishaku']
 
-bot = commands.Bot(command_prefix=get_prefix)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('rin '))
 
 if __name__ == '__main__':
     for extension in extensions:
@@ -28,16 +22,15 @@ if __name__ == '__main__':
             bot.load_extension(extension)
             print(f'Loaded {extension}!')
         except discord.ClientException:
-            print(f'{extension} does not have a setup...')
-        except (ImportError, Exception):
-            print(f'Failed to load {extension}...')
+            print(f'{extension} does not have a setup function...')
+        except (ImportError, Exception) as e:
+            print(f'Failed to load {extension}... ({e})')
 
 
 @bot.event
 async def on_ready():
     print(f'Hello World, I\'m {bot.user.name}!')
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Game('In development!'))
+    await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(name='In development!'))
 
 
-bot.load_extension('jishaku')
 bot.run(config['token'])
